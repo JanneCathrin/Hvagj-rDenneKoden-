e4e8426cd5002e9305e596ff5b47c2f7cc18dc9f
JanneCathrin <77385680+JanneCathrin@users.noreply.github.com>
2022-01-17 00:06:21 +0100

    Add files via upload

---
+++ Hva gjør denne koden/eksempel/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace eksempel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            var range = 250;
+            var counts = new int[range];
+            int totalLetters = 0;
+            string text = "something";
+
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine(); string.Empty;
+                foreach (var character in text.ToUpper())
+                {
+                    totalLetters++;
+                    counts[(int)character]++;
+                }
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {
+                        var character = (char)i;
+                        Console.WriteLine(character + " - " + counts[i]);
+                        var precentage = 100 * (double)counts[i] / totalLetters;
+                        string output = character + " - " + precentage.ToString("F2") + "%";
+                        Console.CursorLeft = Console.BufferWidth - output.Length - 1;
+                        Console.WriteLine(output); 
+                    }
+                }
+            }
+        }
+
+
+    }
+}
